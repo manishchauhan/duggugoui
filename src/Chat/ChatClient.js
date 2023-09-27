6786b461a7f1b76cff03bdf9d5f185ffbd2dadd0
manish chauhan <negi.chauhan@gmail.com>
2023-09-27 20:22:08 +0530

    updated code

--- src/Chat/ChatClient.js
+++ src/Chat/ChatClient.js
@@ -1,41 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import EmojiPicker from 'emoji-picker-react';
+import EmojiPicker, { EmojiStyle, SuggestionMode } from 'emoji-picker-react'; // Update your import
 import '../App.css';
 import './ChatClient.css';
-import { fetchData} from '../Util/http';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { fetchData, getCookie, useFetch } from '../Util/http';
+import { useNavigate } from 'react-router-dom';
+
 export default function ChatClient() {
   const [msg, setMsg] = useState('');
   const [msgPool, setMsgPool] = useState([]);
   const [socket, setSocket] = useState(null);
   const [internalError, setInternalError] = useState('');
-  const [userName,setUserName]=useState(``);
-  const navigate = useNavigate(); // Initialize the navigate function
+  const [userName, setUserName] = useState('');
+  const navigate = useNavigate();
   
   useEffect(() => {
-    const newSocket = new WebSocket('ws://localhost:3001/ws'); // Adjust the URL as needed
-    async function validateLogin(newSocket)
-    {
-        try
-        {
-            const responseData=await fetchData(`http://localhost:8080/user/home/chat`);
-            if (newSocket && responseData.status)
-            {
-               setUserName(responseData.User.username)
-               const welcomeMsg=`Welcome to Chat:  ${responseData.User.username}`;
-               newSocket.send(welcomeMsg);
-            }
-        }catch(error)
-        {
-          setInternalError('Something happen wrong ' + error.message);
-        }
+    async function validateLogin() {
+      try {
+        const responseData = await fetchData('http://localhost:8080/user/home/chat');
+        setUserName(responseData.User.username);
+      } catch (error) {
+        setInternalError('Something happened wrong ' + error.message);
+      }
     }
-   
-   
+    validateLogin();
 
-    newSocket.addEventListener('open', () => {
-      console.log('Connected to server');
-    });
+    const newSocket = new WebSocket('ws://localhost:3001/ws');
+    newSocket.addEventListener('open', () => {});
 
     newSocket.addEventListener('error', (error) => {
       console.log('Connection error:', error);
@@ -47,12 +37,11 @@ export default function ChatClient() {
     });
 
     setSocket(newSocket);
-    validateLogin(newSocket);
+
     return () => {
       newSocket.close();
     };
   }, []);
-
 
   function addMessage() {
     if (socket && msg) {
@@ -60,42 +49,54 @@ export default function ChatClient() {
       setMsg('');
     }
   }
-  async function Logout()
-  {
-    try
-    {
-        const responseData=await fetchData(`http://localhost:8080/user/logout`);
-       if(!responseData.status)
-       {
-        navigate("/")
-       }
-    }catch(error)
-    {
-      setInternalError('Something happen wrong ' + error.message);
+
+  async function Logout() {
+    try {
+      const responseData = await fetchData('http://localhost:8080/user/logout');
+      if (!responseData.status) {
+        navigate('/');
+      }
+    } catch (error) {
+      setInternalError('Something happened wrong ' + error.message);
     }
   }
+
+  function onEmojiSelect(emojiData, event) {
+    // Combine the selected emoji with the existing message
+    setMsg((prevMsg) => prevMsg + emojiData.emoji);
+  }
+
   return (
-    <div className='chat-container'>  
-      <button className="send-button" onClick={Logout}>Logout</button>
-      <div className='room-name'>Room Name: Computers</div>
+    <div className="chat-container">
+      <button className="send-button" onClick={Logout}>
+        Logout
+      </button>
+      <div className="room-name">Room Name: Computers</div>
       {internalError && <div>{internalError}</div>}
-      <div className='chat-box'>
+      <div className="chat-box">
         {msgPool.map((msg, index) => (
-          <span key={index} className='chat-text'>
+          <span key={index} className="chat-text">
             {msg}
           </span>
         ))}
       </div>
-      <div className='message-input'>
+      <div className="message-input">
         <textarea
           value={msg}
-          rows='4'
-          placeholder='Type your message...'
+          rows="4"
+          placeholder="Type your message..."
           onChange={(evt) => {
             setMsg(evt.target.value);
           }}
         />
-        <button className='send-button' onClick={addMessage}>
+        <div className="divClass">
+          <EmojiPicker  height={300} width={200}
+            onEmojiClick={onEmojiSelect}
+            emojiStyle={EmojiStyle.NATIVE}
+            suggestionMode={SuggestionMode.IMPERFECT}
+          />
+        </div>
+        <button className="send-button" onClick={addMessage}>
           Send Message
         </button>
       </div>
